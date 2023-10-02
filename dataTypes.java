4a4f0a96b1ee538f48e2a6ab84f407818983ec66
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-02 15:37:42 -0700

    Datatypes in Java

---
+++ dataTypes.java
@@ -0,0 +1,19 @@
+public class dataTypes {
+    public static void main(String[] args){
+        byte b = 8;
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        boolean d = true;
+        System.out.println(d);
+        int e = 4;
+        System.out.println(e);
+        long f = 8;
+        System.out.println(f);
+        float g = 12;
+        System.out.println(g);
+        double h = 14;
+        System.out.println(h);
+
+    }
+}
