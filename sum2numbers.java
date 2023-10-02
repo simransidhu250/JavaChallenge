b66fec0345610ccf9a2a94a88d34df74b8d7cc06
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-02 15:39:42 -0700

    Sum of Two Numbers

---
+++ sum2numbers.java
@@ -0,0 +1,8 @@
+public class sum2numbers {
+    public static void main(String [] args){
+        int a =10, b =5;
+        int sum =0;
+        sum = a+b;
+        System.out.println(sum);
+    }
+}
