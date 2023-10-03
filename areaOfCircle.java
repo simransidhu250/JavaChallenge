27657548de21deb092b915094d4746ee6424630f
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-02 21:48:04 -0700

    Area of Circle

---
+++ areaOfCircle.java
@@ -0,0 +1,10 @@
+public class areaOfCircle {
+    public static void main(String []args){
+        float pi = 3.14f;
+        float radius = 2f;
+        double area;
+        area = pi*radius*radius;  // area = pi *r^2;
+        System.out.println(area); 
+    }
+    
+}
