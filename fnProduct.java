106ca9141cac0d7c7539750376919434b5b46de3
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-04 10:58:36 -0700

    Fiding prodcuct of numbers using function

---
+++ fnProduct.java
@@ -0,0 +1,14 @@
+public class fnProduct {
+    public static void main(String [] args){
+        int a = 5;
+        int b = 6;
+        int ans = prod(a,b);
+        System.out.println(ans);
+
+    }
+
+    public static int prod(int a, int b){
+        return a*b;
+    }
+    
+}
