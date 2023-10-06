d422cf51e155a0d337c7175fff2e96f8f66abfdc
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-05 21:52:46 -0700

    Comments added

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -5,10 +5,10 @@ public class BinaryToDecimal {
         int result = 0;
         int power = 1;
         while(n>0){
-            digit = n%10;
-            result = result+ power*digit;
-            power = power*2;
-            n=n/10;            
+            digit = n%10; // extracting last number
+            result = result+ power*digit; 
+            power = power*2;  // increasing power to make a shift to the left for the next digit that will be extracted
+            n=n/10;   // new number after removing the extracted digit from the number           
         }
         System.out.println(result);
     }
