0c39757c787cf57cdbfeb03f5b6b28b1e74aea66
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-09 21:18:45 -0700

    Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,13 @@
 public class BinarySearch {
     public static void main(String [] args){
-        int numbers[] = {1,5,9,12,14,18};
+        int numbers[] = {1,5,9,12,14,18};   // Binary prequiste : Array should be sorted
         int key = 12;
-        int index = binarySearch(numbers, key);
-        if(index == -1){
+
+
+        // Binary Search always performs better than Linear Search
+
+        int index = binarySearch(numbers, key);             // Time Complexity is O(log n base 2)  
+        if(index == -1){                                    // Space complexity O(1)
             System.out.println("Key not found");
         }
         else{
@@ -17,14 +21,14 @@ public class BinarySearch {
         end = numbers.length-1;
         
         while(start<=end){
-            mid = (start+end)/2;
-            if(numbers[mid]==key){
+            mid = (start+end)/2;    //diving the array into to half and checking if middle element is the key
+            if(numbers[mid]==key){      
             return mid;
              }
-            else if(numbers[mid]>key){
+            else if(numbers[mid]>key){  // if the key is less than mid it means the element is on left of the middle element
                 end = mid-1;
             }
-            else{
+            else{                   // if the key is greater than mid it means the element is on right of the middle element
                 start = mid+1;
             }
         }
