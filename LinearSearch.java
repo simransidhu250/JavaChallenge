7537534e691c2f6950d29b7ac465c29ecb4df043
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-09 19:07:59 -0700

    Linear Search

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+public class LinearSearch{
+    public static void main(String [] args){
+        int numbers[] ={2,4,6,8,10,12,14,16 };
+        int key =9;
+        int index = linearSearch(numbers,key);
+        if(index==-1){
+            System.out.println("Key not found");
+        }
+        else{
+            System.out.println("Key is at index"+ index);
+        }
+    }
+
+    public static int linearSearch(int numbers[],int key){
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
