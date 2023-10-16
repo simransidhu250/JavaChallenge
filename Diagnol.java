4f00f8aefd5e34fa03401377595891ace1c3ab39
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-16 15:30:34 -0700

    Sum of primary and secondary diagnol in the matrix

--- Diagnol.java
+++ Diagnol.java
@@ -1,13 +1,31 @@
 public class Diagnol {
     public static void main(String [] args){
-        int arr [][] ={{1,2,3,4}, {5,6,7,8}, {9,10,11,12},{13,14,15,16}};
-        diagnolMatrix(arr);
+        int arr [][] ={{1,2,3}, {4,5,6}, {7,8,9}};
+        // diagnolMatrix(arr);
+        sumDiagnolMatrix(arr);
     }
 
     public static void diagnolMatrix(int arr[][]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i][i]+"\t");
         }
+    }
+
+    public static void sumDiagnolMatrix(int arr[][]){
+        int sum =0;
+        for(int i=0; i<arr.length; i++){
+
+            // to find sum of primary diagnol
+            sum= sum+arr[i][i];
+
+            if(arr[i][i]!=arr[i][arr.length-1-i]){      //  if the length of square matrix is odd
+
+                // to find the sum of the secondary diagnol
+                sum = sum + arr[i][arr.length-1-i];
+            }
+
+        }
+        System.out.println(sum);
     }
     
 }
