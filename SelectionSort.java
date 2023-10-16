e5b842c7944460083536f8e960785a8fed1c3103
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-16 13:23:40 -0700

    Selection Sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,10 +6,13 @@ public class SelectionSort {
     }
     public static void selectionSort(int arr[]){
         int minIndex=0, temp =0;
+
+        // Time complexity of Selection sort is O(n^2)
+        // Space complexity is O(1)
         for(int i=0; i<arr.length-1; i++){
             minIndex = i;
             for(int j=i+1; j<arr.length; j++){
-                if(arr[minIndex]>arr[j]){
+                if(arr[minIndex]>arr[j]){                               // we find the minimum  in the array and replaced the particular index with the smallest while traversing 
                     minIndex = j;
                 }
             }
