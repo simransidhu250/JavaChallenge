d513abc8014ab6eb53ac198fa61f973c9754006c
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-10-17 19:30:51 -0700

    Substrings

---
+++ Substring.java
@@ -0,0 +1,16 @@
+public class Substring {
+    public static void main(String []args){
+        String str = "HelloWordl";
+        System.out.println(str.substring(0, 5));  // in-built substring function
+        substr(str, 0, 5); // user defined function
+    }
+
+    public static void substr(String str, int start, int end){
+        String subst="";
+        for(int i=start; i<end; i++){
+            subst += str.charAt(i); 
+        }
+        // System.out.println(subst);
+    }
+    
+}
