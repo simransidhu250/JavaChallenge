03eeb1117ce46be275bad5a95305046fe205fd49
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-11-09 08:53:56 -0800

    Multilevel Inheritance in Java

--- MultiLevelInheritance.java
+++ MultiLevelInheritance.java
@@ -2,7 +2,7 @@ public class MultiLevelInheritance{
     public static void main(String [] args){
        Dog dobby = new Dog();
        dobby.eat();
-       dobby.legs = 4;
+       dobby.legs = 4;      // Derived class inherits all the properties and methods from the parent class
        System.out.println(dobby.legs);
 
     }
