afa31b0cf6b54dfe38b3663e3979261fc1f6bb40
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-11-09 08:52:58 -0800

    Multilevel Inheritance in Java

---
+++ MultiLevelInheritance.java
@@ -0,0 +1,28 @@
+public class MultiLevelInheritance{
+    public static void main(String [] args){
+       Dog dobby = new Dog();
+       dobby.eat();
+       dobby.legs = 4;
+       System.out.println(dobby.legs);
+
+    }
+}
+
+class Animals{
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathe");
+    }
+}
+
+class Mammal extends Animal{
+    int legs;
+
+}
+
+class Dog extends Mammal{
+    String breed;
+}
