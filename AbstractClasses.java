23ee0646e3ab165371db84d43637fb1770a3e778
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-11-12 10:02:35 -0800

    Abstract Class in Java

---
+++ AbstractClasses.java
@@ -0,0 +1,32 @@
+public class AbstractClasses {
+    public static void main(String []args){
+        Horse h1 = new Horse();
+        h1.walk();  // implements the functionality of the abstract function walk
+        h1.eats();  // inherits eats function from parent class Animal
+        System.out.println(h1.color);   // first the constructor of the parent class is called and color is assigned brown
+    }    
+}
+
+
+abstract class Animal{
+    String color;
+    Animal(){
+        color = "brown";
+    }
+
+    void eats(){
+        System.out.println("eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal{
+    void changeColor(){
+        color = "dark brown";
+    }
+
+    void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
