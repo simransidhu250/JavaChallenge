60277048b47b253a635a5ca6e3109b45dc400898
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-12-03 19:34:44 -0800

    Finding the last occurence of key in the given array

---
+++ RecursionLastOccurence.java
@@ -0,0 +1,21 @@
+public class RecursionLastOccurence {
+    public static void main(String args []){
+        int arr[] = {8,3,6,9,5,10,2,5,3};
+        int index = lastOccurence(arr, 0, 5);
+        System.out.println("Key is at index: "+index);
+    }
+    
+    static int lastOccurence(int arr[], int i, int key){
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound  = lastOccurence(arr, i+1, key);
+        if(isFound == -1 && arr[i] == key){     // checking that the whole array has been traversed and the key is found at some position
+            return i;
+        }
+
+        return isFound;
+
+        
+    }
+}
