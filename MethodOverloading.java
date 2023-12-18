ac06321c19e6619105ceb653e561cdb80b3b5955
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-12-18 10:31:05 -0800

    Method Overloading

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -2,7 +2,7 @@ public class MethodOverloading {
     public static void main(String args[]){
         Calculator calc = new Calculator();
 
-        // Compile Time pzloymorphism
+        // Compile Time polymorphism
         System.out.println(calc.sum(1, 2));
         System.out.println(calc.sum(1.3f, 2.2f));
         System.out.println(calc.sum(1, 2, 3));
