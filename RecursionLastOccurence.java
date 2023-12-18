825e1fde4329ffe23116c61dc3242c3c8d6fced9
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-12-18 10:28:27 -0800

    Find position of last occurence of key in arrary

--- RecursionLastOccurence.java
+++ RecursionLastOccurence.java
@@ -9,13 +9,16 @@ public class RecursionLastOccurence {
         if(i == arr.length){
             return -1;
         }
+
+        // Time Complexity of O(n)
+        // Space complexity is space occupied by function calls O(n)
+
         int isFound  = lastOccurence(arr, i+1, key);
         if(isFound == -1 && arr[i] == key){     // checking that the whole array has been traversed and the key is found at some position
             return i;
-        }
+        }               
 
         return isFound;
-
         
     }
 }
