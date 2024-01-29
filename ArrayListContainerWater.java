87b803a3f2e60fad5bf495918c6812d95ae4de96
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-01-29 09:42:44 -0800

    Optimized approach

--- ArrayListContainerWater.java
+++ ArrayListContainerWater.java
@@ -24,7 +24,21 @@ public class ArrayListContainerWater {
 
         System.out.println(heights);
 
-        // Stores the maximum water
+
+        // Brute Force Approach
+
+        // bruteForce(heights);
+
+        // Two Pointer Approach
+        optimized(heights);
+
+        
+    }
+
+
+    // Brute force approach - O(n^2)
+
+    public static void bruteForce(ArrayList <Integer> heights){
 
         int max =  Integer.MIN_VALUE;
         int capacity = 0;
@@ -42,6 +56,36 @@ public class ArrayListContainerWater {
             }
         }
 
-        System.out.println("Maximum water contained: "+max);
+        System.out.println("Maximum water contained is : "+max);
+    }
+
+    // Optimized approach - Two Pointer Approach - O(n)
+
+    public static void optimized(ArrayList <Integer> heights){
+        int frontPt = 0;
+        int backPt = heights.size()-1;
+        int max = Integer.MIN_VALUE;
+        int capacity = 0;
+        int height = heights.get(0);
+        int width = 0;
+
+        while(frontPt<backPt){
+
+            // Calculate area
+            height = Math.min(heights.get(frontPt),heights.get(backPt));
+            width = backPt - frontPt;
+            capacity = height*width;
+            max = Math.max(capacity,max);
+
+            //Update pointers
+
+            if(heights.get(frontPt)<heights.get(backPt)){        
+                frontPt++;
+            }
+            else{
+                backPt--;
+            }
+        }
+        System.out.println("Maximum water contained is: "+max);
     }
 }
