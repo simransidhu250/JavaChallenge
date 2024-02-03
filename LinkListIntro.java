bac21391cf2392a40782bd9f0ff29a6a0c5adafc
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-02 20:06:17 -0800

    Link List Add Node at head

--- LinkListIntro.java
+++ LinkListIntro.java
@@ -17,7 +17,31 @@ public class LinkListIntro {
 
     }
 
-    
+    // Add element at the head
+
+    public static void addNodeAtHead(int data){
+        
+        // create a new node
+
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            return;
+        } 
+
+        // set the link of new node to current head
+
+        newNode.next = head;
+
+        // set the head node to the new node
+
+        head = newNode;
+    }
+
+
+
 
 
 
