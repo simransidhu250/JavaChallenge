56c22452dd3d5e87063bc1fe04f48fe518d54c37
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-02 20:05:30 -0800

    Link List Node

---
+++ LinkListIntro.java
@@ -0,0 +1,26 @@
+public class LinkListIntro {
+    public static void main(String args[]){
+        LinkListIntro ll = new LinkListIntro();
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    
+
+
+
+
+    
+}
