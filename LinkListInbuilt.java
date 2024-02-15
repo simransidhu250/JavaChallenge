f88fb8d8f3b6bc63458a79a669f99828835a795f
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-14 21:41:26 -0800

    In-built Linked List

---
+++ LinkListInbuilt.java
@@ -0,0 +1,22 @@
+// import linked list
+import java.util.LinkedList;
+
+public class LinkListInbuilt {
+    public static void main(String args){
+
+        // Create a Linked List
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        // Add elements to Link List
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(0);
+
+        // print the list
+        System.out.println(ll);
+
+        
+    }
+
+    
+}
