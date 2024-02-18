ebe62605c3887c4000b2ae31efddacc6732d6edf
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-17 17:09:57 -0800

    create link list

--- LinkListMergeSort.java
+++ LinkListMergeSort.java
@@ -1,6 +1,15 @@
 import java.util.LinkedList;
 public class LinkListMergeSort {
     public static void main(String args[]){
+
+        // Create Link List
         LinkedList<Integer> ll = new LinkedList<>();
+
+        // Add elements to Link List
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
     }
 }
