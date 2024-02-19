427181ad3c8d0f2a053a1d17920859cb723d89e6
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-18 22:25:49 -0800

    Add elements to the link list

--- LinkListZigZag.java
+++ LinkListZigZag.java
@@ -5,5 +5,15 @@ public class LinkListZigZag {
     // Create linked list
     LinkedList<Integer> ll = new LinkedList();
 
+    // Add elements to link list
+    ll.addFirst(4);
+    ll.addFirst(5);
+    ll.addFirst(6);
+    ll.addFirst(3);
+    ll.addFirst(2);
+    ll.addFirst(1);
+
+
+
    } 
 }
