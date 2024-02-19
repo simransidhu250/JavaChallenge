d00e5112e21fe66f16567a0f982d0c0d25df9a07
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-18 22:12:30 -0800

    Zig-Zag pattern of link list

--- LinkListZigZag.java
+++ LinkListZigZag.java
@@ -2,5 +2,8 @@ import java.util.LinkedList;
 public class LinkListZigZag {
    public static void main(String args[]){
 
+    // Create linked list
+    LinkedList<Integer> ll = new LinkedList();
+
    } 
 }
