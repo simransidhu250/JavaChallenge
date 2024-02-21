8a1a412ea5f43d48819d12f281dc93bafae71855
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-20 16:29:16 -0800

    Doubly link list create a node

---
+++ LinkListDoubly.java
@@ -0,0 +1,22 @@
+public class LinkListDoubly {
+    public static ListNode head = null;
+    public static ListNode tail = null;
+    public static int size = 0;
+    public static void main(String args[]){
+
+    }
+
+    // Node class
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode prev;
+    }
+
+    public static void addToHead(){
+        if(head == null){
+
+        }
+
+    }
+}
