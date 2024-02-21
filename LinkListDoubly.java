b97f0ad6aca32377ffe14d0f9962c3b83bb9e146
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-20 16:42:47 -0800

    Doubly Linked List

--- LinkListDoubly.java
+++ LinkListDoubly.java
@@ -3,20 +3,42 @@ public class LinkListDoubly {
     public static ListNode tail = null;
     public static int size = 0;
     public static void main(String args[]){
-
+        LinkListDoubly ll = new LinkListDoubly();
+        ll.addToHead(4);
     }
-
+//--------------------Node class----------------------------------------------
     // Node class
-    public class ListNode{
+    static class ListNode{
         int val;
         ListNode next;
         ListNode prev;
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+//---------------------Add node to head --------------------------------------------------
+
+    public static void addToHead(int val){
+        ListNode temp = new ListNode(val);
+        if(head == null){
+            head = temp;
+            return;
+        }
+        temp.next = head;
+        head.prev = temp;
+        head = temp;
     }
 
-    public static void addToHead(){
-        if(head == null){
+//--------------------Print the doubly linked list--------------------------------------
 
+    public void printList(){
+        ListNode temp = head;
+
+        while(temp!=null){
+            System.out.print(temp.val+"  ");
+            temp = temp.next;
         }
-
     }
 }
