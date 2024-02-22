944c080171b0e65e84eebe41ec9fc2d8a529abc9
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-21 16:46:41 -0800

    Find the intersection point of two linked list

--- LinkListIntersectionPoint.java
+++ LinkListIntersectionPoint.java
@@ -12,21 +12,60 @@ public class LinkListIntersectionPoint {
         headA = l1.addToHead(headA, 4);
 
         // print the linked list
-        l1.printList(headA);
+        // l1.printList(headA);
 
-        // create the linked list
-        LinkListIntersectionPoint l2 = new LinkListIntersectionPoint();
         headB = headA.next.next;
-        headB = l2.addToHead(headB, 3);
-        headB = l2.addToHead(headB, 2);
-        headB = l2.addToHead(headB, 1);
+        headB = l1.addToHead(headB, 3);
+        headB = l1.addToHead(headB, 2);
+        headB = l1.addToHead(headB, 1);
 
         // print the linked list
-        l1.printList(headB);
+        // l1.printList(headB);
+
+        System.out.println(l1.getIntersectionNode().val);
         
+    }
 
+//---------------------- Intersection Point--------------------------------
 
+    public ListNode getIntersectionNode(){
+        // get the size
+        int sizeA = getCount(headA);
+        int sizeB = getCount(headB);
+        
+        // Skip some nodes to bring both heads to equal lengths
+        while (sizeA>sizeB) {
+            headA =headA.next;
+            sizeA--;
+        }
+        while (sizeB>sizeA) {
+            headB =  headB.next;
+            sizeB--;
+        }
 
+        // Check if they meet
+        while (headA!=headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        
+        // Check if there is intersection point or not
+        if(headA==null){
+            return null;
+        }
+        else{
+            return headA;
+        }
+    }
+
+// --------------------Count number of nodes---------------------------
+    public static int getCount(ListNode head){
+        int count=0;
+        while(head!=null){
+            count++;
+            head = head.next;
+        }
+        return count;
     }
 //--------------------Node class----------------------------------------------
     // Node class
