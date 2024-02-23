4e55cf0298c157354f77266fbaa655086956cdd2
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-22 22:15:02 -0800

    Implement Stack Using Linked List

---
+++ StackUsingLinkedList.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+public class StackUsingLinkedList {
+
+    public static LinkedList<Integer> ll = new LinkedList<>();
+    public static void main(String args[]){
+
+        // Creat an instance of the class
+        StackUsingLinkedList stack = new StackUsingLinkedList();
+
+        // Add elements the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        // print stack
+        System.out.println(ll);
+
+        // Remove element
+        stack.pop();
+
+         // print stack
+         System.out.println(ll);
+
+         // Peek
+         System.out.println(stack.top());
+
+    }
+
+    public void push(int val){
+        ll.addFirst(val);
+    }
+
+    public int top(){
+        if(ll==null){
+            return -1;
+        }
+        return ll.getFirst();
+    }
+
+    public int pop(){
+        if(ll==null){
+            return -1;
+        }
+        int element  = ll.getFirst();
+        ll.poll();
+        return element;
+
+    }
+
+}
