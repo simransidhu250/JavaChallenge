00d605dc6e91041739cc83bf249f506172681000
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-02-26 23:28:58 -0800

    Create a stack

--- StackStockSpan.java
+++ StackStockSpan.java
@@ -1,6 +1,23 @@
+import java.util.Stack;
 public class StackStockSpan {
     public static void main(String args[]){
-        
+        StackStockSpan sp = new StackStockSpan();
+        // In-built stack
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        printStack(stack);
+
+
     }
+        // print function
+        public static void printStack(Stack<Integer> stack){
+            while (!stack.empty()) {
+                System.out.println(stack.pop());
+            }
+        }
     
 }
