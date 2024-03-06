ba04380f57f2d96d7ee9a56103b1d13a703e05ad
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-03-05 22:04:42 -0800

    Stock Span problem

--- StackStockSpan.java
+++ StackStockSpan.java
@@ -1,29 +1,40 @@
 import java.util.Stack;
+
 public class StackStockSpan {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         StackStockSpan sp = new StackStockSpan();
 
-
-        int arr[] = {100,80,60,70,60,85,100};
+        int arr[] = { 100, 80, 60, 70, 60, 85, 100 };
         sp.stockSpan(arr);
-
-
 
     }
 
-    public Stack<Integer> stockSpan(int arr[]){
+    public void stockSpan(int arr[]) {
         Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<arr.length; i++){
-            while(stack.empty()|| )
+        int span[] = new int[arr.length];
+        stack.push(0);
+        span[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            System.out.println(arr[stack.peek()] + " < " + arr[i]);
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                stack.pop();
+            }
+            if (stack.empty()) {
+                span[i] = i;
+            } else {
+                span[i] = i - stack.peek();
+            }
+            stack.push(i);
+
+            System.out.print(span[i] + "\t");
         }
-        return stack;
     }
 
     // print function
-    public static void printStack(Stack<Integer> stack){
+    public static void printStack(Stack<Integer> stack) {
         while (!stack.empty()) {
-            System.out.println(stack.pop());
+            System.out.print(stack.pop());
         }
     }
-    
+
 }
